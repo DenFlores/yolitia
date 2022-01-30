377f61b400fa09ae1fc2dedd10ef9420f0355d81
Abraham David <abrahamdavid.ramirez93@gmail.com>
2022-01-29 19:34:05 -0600

    Se agregó documento Js con la creación de objetos por cada producto del catpalogo

---
+++ assets/js/productos.js
@@ -0,0 +1,36 @@
+
+function Productos (nombre, precio){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponibilidad = true;
+}
+
+const producto1 = new Productos("Llavero de pinguino", 500);
+const producto2 = new Productos("Llavero de osito", 500);
+const producto3 = new Productos("Llavero osito polar", 500);
+const producto4 = new Productos("Pantuflas peluche", 500);
+const producto5 = new Productos("Garras Oso", 500);
+const producto6 = new Productos("Gorro Oso", 500);
+const producto7 = new Productos("Pijama oso", 500);
+const producto8 = new Productos("Gorro conejo", 500);
+const producto9 = new Productos("Llavero gato", 500);
+
+const producto1 = new Productos("Llavero de pinguino", 500);
+const producto2 = new Productos("Llavero de osito", 500);
+const producto3 = new Productos("Llavero osito polar", 500);
+const producto4 = new Productos("Pantuflas peluche", 500);
+const producto5 = new Productos("Garras Oso", 500);
+const producto6 = new Productos("Gorro Oso", 500);
+const producto7 = new Productos("Pijama oso", 500);
+const producto8 = new Productos("Gorro conejo", 500);
+const producto9 = new Productos("Llavero gato", 500);
+
+const producto1 = new Productos("Llavero de pinguino", 500);
+const producto2 = new Productos("Llavero de osito", 500);
+const producto3 = new Productos("Llavero osito polar", 500);
+const producto4 = new Productos("Pantuflas peluche", 500);
+const producto5 = new Productos("Garras Oso", 500);
+const producto6 = new Productos("Gorro Oso", 500);
+const producto7 = new Productos("Pijama oso", 500);
+const producto8 = new Productos("Gorro conejo", 500);
+const producto9 = new Productos("Llavero gato", 500);
